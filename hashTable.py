6ac24bbf5d1c097893e7f39bbb0cf3495c384815
g0tie <gaultierbegue@outlook.com>
2022-05-07 11:03:40 +0400

    finishing hashTable demo

--- hashTable.py
+++ hashTable.py
@@ -1,5 +1,26 @@
+
 def hashFunc(str):
-    return sum([ord(str[i]) for i in range(len(str))]) % 100
+    return sum([ord(str[i].lower()) ** 3 + ord(str[i].upper()) for i in range(len(str))]) % 1000000
 
 
-print(hashFunc("github"));
+def makeHashTable(keys, values):
+    hashTable = [0] * 1000000
+    
+    for i in range(len(keys)):
+        key = keys[i]
+        val = values[i]
+        
+        index = hashFunc(key) 
+        hashTable[index] = val
+    
+    return hashTable
+        
+        
+extensions = ["jpeg", "pdf", "svg", "ppt", "zip", "mpeg"]
+mimeType = ["image/jpeg", "txt/pdf", "image/svg+xml", "application/vnd.ms-powerpoint","application/zip", "video/mpeg"]    
+
+hashTable = makeHashTable(extensions, mimeType)
+search = "svg" 
+
+print("Generated hash: {}".format(hashFunc(search))	)
+print("Corresponding mime Type: {}".format(hashTable[hashFunc(search)]))
